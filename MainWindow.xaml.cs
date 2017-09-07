620fd708b51404afc8da8971d9f8ec54300a2e8b
Rostyslav <ptashinskij.rostik@yandex.ua>
2017-09-07 20:39:53 +0300

    pars1
    Parsing product catalog

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace Diplom_Parser
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HtmlDocument doc = new HtmlDocument();
+        Parser parser;
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*parser = new Parser();
+            doc = parser.Get_Catalog();
+            textBlock1.Text = doc.DocumentNode.OuterHtml;
+            /* StreamWriter sw = new StreamWriter(new FileStream("FileName.txt", FileMode.Create, FileAccess.Write));
+             sw.Write(textBlock1.Text);
+             sw.Close();*/
+           // parser.Get_Product_Description(doc);
+        }
+
+        private void menu_icon_png_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (HamburgerMenu.Margin.Left < 0)
+            {
+                HamburgerMenu.Margin = new Thickness(0, 0, 0, 0);
+                menu_icon_png.Source = new BitmapImage(new Uri(@"images\arrow_left_icon.png", UriKind.Relative));
+            }
+            else
+            {
+                HamburgerMenu.Margin = new Thickness(-200, 0, 0, 0);
+                menu_icon_png.Source = new BitmapImage(new Uri(@"images\menu_icon.png", UriKind.Relative));
+            }
+        }
+    }
+}
---
+++ Parser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.IO;
+
+namespace Diplom_Parser
+{
+    class Parser
+    {
+        public Parser()
+        {
+
+        }    
+        //32 товара
+        public HtmlDocument Get_Catalog()
+        {
+            var url = "https://rozetka.com.ua/ua/mobile-phones/c80003/preset=smartfon/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            return doc;
+        }
+        //кожен товар з 32
+        public void Get_Product_Description(HtmlDocument doc)
+        {
+            var Node = doc.DocumentNode
+                          .Descendants("div")
+                          .Where(d => d.Attributes.Contains("class") &&
+                          d.Attributes["class"].Value=="g-i-tile g-i-tile-catalog");
+
+            StreamWriter sw = new StreamWriter(new FileStream("NodeInfoText.txt", FileMode.Create, FileAccess.Write));
+            foreach (var n in Node)
+            {
+                sw.Write("\n //////////// \n");
+                sw.Write(n.InnerText);
+                sw.Write("\n //////////// \n");
+                
+            }
+            sw.Close();
+        }
+
+        public void Get_Product_Reviews(string Hrml_code)
+        {
+
+        }
+    }
+}
